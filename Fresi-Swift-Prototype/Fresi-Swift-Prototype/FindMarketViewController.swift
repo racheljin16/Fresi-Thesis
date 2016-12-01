1d7eb3f492a604be07db267cd0c5a620ff1e2020
Rachel <Rachel@Rachel-Jin.local>
2016-11-30 18:33:26 -0800

    Update homepage and market page

---
+++ Fresi-Swift-Prototype/Fresi-Swift-Prototype/FindMarketViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FindMarketViewController.swift
+//  Fresi-Swift-Prototype
+//
+//  Created by Rachel Jin on 11/30/16.
+//  Copyright © 2016 Rachel Jin. All rights reserved.
+//
+
+import UIKit
+
+class FindMarketViewController: UIViewController, UIScrollViewDelegate {
+
+
+    @IBOutlet weak var homeSearchView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = imageView.frame.size
+        scrollView.delegate = self
+        self.homeSearchView.alpha = 0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        scrollView.contentInset.top = 0
+        scrollView.contentInset.bottom = 50
+        scrollView.scrollIndicatorInsets.top = 0
+        scrollView.scrollIndicatorInsets.bottom = 50
+        
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView){
+        
+        let offset = Float(scrollView.contentOffset.y)
+        
+        if offset >= 200 {
+            UIView.animate(withDuration: 1, animations: {
+                self.homeSearchView.alpha = 1
+            })
+        } else {
+            self.homeSearchView.alpha = 0
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fresi-Swift-Prototype/Fresi-Swift-Prototype/MarketViewController.swift
@@ -0,0 +1,66 @@
+//
+//  MarketViewController.swift
+//  Fresi-Swift-Prototype
+//
+//  Created by Rachel Jin on 11/30/16.
+//  Copyright © 2016 Rachel Jin. All rights reserved.
+//
+
+import UIKit
+
+class MarketViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var heroScrollView: UIScrollView!
+    @IBOutlet weak var listScrollView: UIScrollView!
+    @IBOutlet weak var listImageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        heroScrollView.contentSize = CGSize(width: 1500, height: 211)
+        heroScrollView.delegate = self
+        listScrollView.contentSize = CGSize(width: 275, height: 2344)
+        listScrollView.delegate = self
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let page : Int = Int(round(heroScrollView.contentOffset.x / 375))
+        pageControl.currentPage = page
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        listScrollView.contentInset.top = 0
+        listScrollView.contentInset.bottom = 50
+        listScrollView.scrollIndicatorInsets.top = 0
+        listScrollView.scrollIndicatorInsets.bottom = 50
+        
+    }
+    
+    
+    @IBAction func didTapBack(_ sender: UIButton) {
+        
+                navigationController?.popToRootViewController(animated: true)
+         
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
